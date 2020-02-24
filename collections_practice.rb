135ac972d3905ea0200e79a07776a88007eb0cb0
Samantha Barros <samantha.barros95@gmail.com>
2020-02-24 00:12:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,6 +33,7 @@ def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
+end
   
   def sum_array(array)
   sum = 0
@@ -48,7 +49,6 @@ def sum_array(array)
     sum+=num
   end
   sum
-end
 end
 
 def add_s(array)
